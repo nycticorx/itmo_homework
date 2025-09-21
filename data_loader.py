1f1fd758d42033ba9e5a6f79b96d944580c90f5a
nycticorx <malinaelvina602@gmail.com>
2025-09-21 12:30:08 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,8 @@
 import pandas as pd
+
 FILE_ID = "1JoUNSGst7mIbon9LpFjIeIdpjuVVPpcg"
 file_url = f"https://drive.google.com/uc?id={FILE_ID}"
-raw_data = pd.read_csv(file_url)
-print(raw_data.head(10))
+
+raw_data = pd.read_csv(file_url)     
+
+print(raw_data.head(10))             
