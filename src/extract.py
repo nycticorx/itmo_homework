7a2d37ffd1e5d91fc873bca301f4f9c8bf50672d
nycticorx <malinaelvina602@gmail.com>
2025-10-31 19:05:01 +0300

    Delete src/extract.py

--- src/extract.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-FILE_ID = "1LtthrtKpMg1cPQEDkKJjglbqGIUDFjYr"
-file_url = f"https://drive.google.com/uc?id={FILE_ID}"
-
-raw_data = pd.read_csv(file_url)
-
-print(raw_data.head(10))    # Вывод первых 10 строк;
